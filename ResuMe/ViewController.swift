20cd86ae8057d5fe7e2c87a1fbeb2857dd53c6f3
Yi Zheng <yizheng@yis-macbook-pro.local>
2019-01-01 20:30:52 -0700

    Initial Commit

---
+++ ResuMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResuMe
+//
+//  Created by Yi Zheng on 2019-01-01.
+//  Copyright © 2019 Yi Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
