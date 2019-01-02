7c7eba015476348de5da22a88ca03b8a39b8decc
Yi Zheng <yizheng@yis-macbook-pro.local>
2019-01-01 21:34:16 -0700

    Project init and set up Firebase

---
+++ Pods/Target Support Files/Pods-ResuMe/Pods-ResuMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ResuMe : NSObject
+@end
+@implementation PodsDummy_Pods_ResuMe
+@end
---
+++ ResuMe/Utils/CustomImageView.swift
@@ -0,0 +1,50 @@
+//
+//  CustomImageView.swift
+//  ResuMe
+//
+//  Created by Yi Zheng on 2019-01-01.
+//  Copyright © 2019 Yi Zheng. All rights reserved.
+//
+
+import UIKit
+
+var imageCache = [String: UIImage]()
+
+class CustomImageView: UIImageView {
+    
+    var lastURLUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        lastURLUsedToLoadImage = urlString
+        
+        //only loading image once
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            
+            if url.absoluteString != self.lastURLUsedToLoadImage {
+                return
+            }
+            
+            guard let imageData = data else { return }
+            
+            let photoImage = UIImage(data: imageData)
+            
+            imageCache[url.absoluteString] = photoImage
+            
+            DispatchQueue.main.async {
+                self.image = photoImage
+            }
+            
+            }.resume()
+    }
+}
