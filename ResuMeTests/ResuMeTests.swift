afe80818b5165243a124eec18f56b14535497180
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-07-22 10:35:14 -0600

    Added XCTest

--- ResuMeTests/ResuMeTests.swift
+++ ResuMeTests/ResuMeTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import ResuMe
 
 class ResuMeTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
@@ -23,6 +23,7 @@ class ResuMeTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
     func testPerformanceExample() {
